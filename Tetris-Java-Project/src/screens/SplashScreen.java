0679d3e0a4f9b7cc0311443042fd62ffabddf126
h669791 <669791@stud.hvl.no>
2024-08-25 12:17:07 +1000

    Fixed the splashscreen displaying splash image, and some warnings in SplashScreen.class

--- Tetris-Java-Project/src/screens/SplashScreen.java
+++ Tetris-Java-Project/src/screens/SplashScreen.java
@@ -1,24 +1,40 @@
 package screens;
+
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
+
 public class SplashScreen extends JWindow {
-    private int duration;
+    final private int duration;
+
     public SplashScreen(int duration) {
         this.duration = duration;
     }
+
     public void showSplash() {
         JPanel content = (JPanel) getContentPane();
         content.setBackground(Color.white);
 // Set the window's bounds, centering the window
-        int width = 600;
-        int height = 800;
+        final int width = 600;
+        final int height = 800;
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (screen.width - width) / 2;
         int y = (screen.height - height) / 2;
         setBounds(x, y, width, height);
 // Build the splash screen
-        JLabel label = new JLabel(new
-                ImageIcon(this.getClass().getResource("/resources/tetrissplashresized.png")));
+        /*JLabel label = new JLabel(new
+                ImageIcon(this.getClass().getResource("/resources/tetrissplashresized.png")));*/
+
+        URL imageUrl = getClass().getClassLoader().getResource("tetrissplashresized.png");
+        JLabel label = null;
+        if (imageUrl == null) {
+            System.err.println("Image not found!");
+        } else {
+            label = new JLabel(new ImageIcon(imageUrl));
+            // Add the label to your layout as needed
+        }
+
+
         JLabel copyrt = new JLabel("Copyright 2024, Group 12 Tetris Game",
                 JLabel.CENTER);
         copyrt.setFont(new Font("Sans-Serif", Font.BOLD, 12));
@@ -36,10 +52,12 @@ public class SplashScreen extends JWindow {
         }
         setVisible(false);
     }
+
     public void showSplashAndExit() {
         showSplash();
         System.exit(0);
     }
+
     public static void main(String[] args) {
 // Show the splash screen
         SplashScreen splash = new SplashScreen(3000);
